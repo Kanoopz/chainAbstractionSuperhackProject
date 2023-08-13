65f30eef1b27a826174a8bc25adb80acc1b790fc
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-08-13 10:42:09 -0600

    Update abstractedAccount.sol

--- smartContracts/layerZeroChainAbstraction/abstractedAccount.sol
+++ smartContracts/layerZeroChainAbstraction/abstractedAccount.sol
@@ -4,38 +4,25 @@ import { IWorldID } from './interfaces/IWorldID.sol';
 import { iNft } from './interfaces/iNft.sol';
 import { iLzErc20 } from './interfaces/iLzErc20.sol';
 
-pragma solidity 0.8.10;
+import { ByteHasher } from "./helpers/ByteHasher.sol";
+import { IWorldID } from "./interfaces/IWorldID.sol";
+
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import { NonblockingLzApp } from "../layerZeroSolidityIntegration/contracts/lzApp/NonblockingLzApp.sol";
 
+pragma solidity 0.8.10;
+
 contract abstractedAccount is NonblockingLzApp
 {
-    ////////////////////////////////////////////////////////////////////////////
-    /////"modifiers"////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////// 
-    /*
-    modifier onlyOwner()
-    {
-        require(msg.sender == owner, "notOwner.");
-        _;
-    }
-    */
-
-    /*
-        modifier onlyOwnerOrWorldId()
-        {
-            
-        }
-    */
+    using ByteHasher for bytes;
 
     ////////////////////////////////////////////////////////////////////////////
     /////"generalVariables"/////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////// 
-    //address public owner;
     address public factoryAddress;
     address public crossChainAaAddress;
-    //DOMINIO//  
-    address public usdTokenAddress; //USDTOKEN ADDRESS ON THIS CHAIN//
+    address public usdTokenAddress; 
 
     uint8 public nftCounter;
     uint8 public tokenCounter;
@@ -44,41 +31,32 @@ contract abstractedAccount is NonblockingLzApp
     address[] public tokenAddresses;
 
     /////"worldcoinVariables"///////////////////////////////////////////////////
-    bool public isWorldIdSet;
-    uint256 public ownerWorldIdNullifierHash;
+    error InvalidNullifier();
 
-    //IWorldID internal immutable worldId;
-    //uint256 internal immutable externalNullifierHash;
-    //uint256 internal immutable groupId = 1;
-    IWorldID internal worldId;
-    uint256 internal externalNullifierHash;
-    uint256 internal immutable groupId = 1;
+    IWorldID internal immutable worldId;
+    uint256 internal immutable externalNullifier;
+    uint256 internal immutable groupId = 0;
+
+    mapping(uint256 => bool) internal nullifierHashes;
+
+    uint public signalCounter = 0;
+    uint256 public ownerNullifierHash;
+    
 
     ////////////////////////////////////////////////////////////////////////////
     /////"functions"////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////  
-    //DOMINIO DE LA CCAA)
-    constructor(address paramOwner, address paramFactoryAddress, address _lzEndpoint, address srcChainUsdTokenAddress) NonblockingLzApp(_lzEndpoint)
+    constructor(address paramFactoryAddress, address _lzEndpoint, address srcChainUsdTokenAddress, address _worldId, string memory _appId, string memory _actionId) NonblockingLzApp(_lzEndpoint)
     {
-        //owner = paramOwner;
         factoryAddress = paramFactoryAddress;
         usdTokenAddress = srcChainUsdTokenAddress;
+
+        IWorldID worldIdInstance = IWorldID(_worldId);
+        
+        worldId = worldIdInstance;
+        externalNullifier = abi.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId).hashToField();
     }
 
-    /*
-        function setWorldId(uint256 variables) public onlyOwner
-        {
-            //goerliOptimism chainId = 420.///
-            //require(msg.sender == owner || msg.sender == worldId, "worldIdSetter not permitted.");
-            require(block.chainid == 5 || block.chainid == 420, "wolrdId not available on this blockchain.");
-            isWorldIdSet = true;
-        }
-
-        function getChainId() public view returns(uint256)
-        {
-            return block.chainid;
-        }
-    */
 
     /////"aaRelatedFunctions"//////////////////////////////////////////////////////////////////////////////
     
@@ -97,39 +75,22 @@ contract abstractedAccount is NonblockingLzApp
         usdTokenAddress = paramUsdTokenAddress;
     }
 
-    /////"layerZeroIntegrationRelatedFunctions"////////////////////////////////////////////////////////////    
+    /////"worldIdVersionOfImportantFunctions"/////////////////////////////////////////////////////////////////////////
 
-    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory _srcAddress, uint64 _nonce, bytes memory _payload) internal override 
+    function setOwnerNullifierHash(string memory signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public
     {
-        //bytes memory constructedPayload = abi.encode(msg.sender, paramNumberToTransfer);
-        //(_tokenAddressPlaceHolder, _quantityToTransferPlaceHolder) = abi.decode(_payload, (address, uint));
+        worldId.verifyProof(root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof);
 
-        (address addressToTransfer, uint256 quantityToTransfer) = abi.decode(_payload, (address, uint256));
-        /*
-            uint256 actualAddressBalance = _balances[addressToTransfer];
-            uint256 newAddressBalance = actualAddressBalance + quantityToTransfer;
-            _balances[addressToTransfer] = newAddressBalance;
-        */
+        ownerNullifierHash = nullifierHash;
 
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////<
-
-        iLzErc20(usdTokenAddress).transfer(addressToTransfer, quantityToTransfer);
+        signalCounter++;
     }
 
-
-
-
-
-    function estimateFee(uint16 _dstChainId, bytes calldata _payload, bool _useZro, bytes calldata _adapterParams) public view returns (uint nativeFee, uint zroFee) 
+    function useWorldIdForChainAbstractionTransferFunction(address paramDestinationAddressToTransfer, uint256 paramQuantityToTransferFromSourceAddress, uint256 paramQuantityToTransferFromDestinationAddress, uint16 _dstChainId, string memory signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public payable
     {
-        return lzEndpoint.estimateFees(_dstChainId, address(this), _payload, _useZro, _adapterParams);
-    }
+        worldId.verifyProof(root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof);
+        signalCounter++;
 
-    //IF NOT SUCCESFULL, USING A WRAPPER THAT CALLS THIS FUNCTION WITH THE VALUE OF 1 ETHER///
-    function chainAbstractionTransferFunction(address paramDestinationAddressToTransfer, uint256 paramQuantityToTransferFromSourceAddress, uint256 paramQuantityToTransferFromDestinationAddress, uint16 _dstChainId) public payable 
-    {
-        //bytes memory constructedPayloadSource = abi.encode(paramDestinationAddressToTransfer, paramQuantityToTransferFromSourceAddress);
         bytes memory constructedPayloadDestination = abi.encode(paramDestinationAddressToTransfer, paramQuantityToTransferFromDestinationAddress);
 
         uint16 valueOne = 1;
@@ -139,32 +100,97 @@ contract abstractedAccount is NonblockingLzApp
         uint etherToSend = 0.5 ether;
 
 
+        _lzSend(_dstChainId, constructedPayloadDestination, payable(tx.origin), address(0x0), adapterParams, etherToSend);
+        iLzErc20(usdTokenAddress).layerZeroTransfer{value: etherToSend}(paramDestinationAddressToTransfer, paramQuantityToTransferFromSourceAddress, _dstChainId);
+    }
+
+
+    function useWorldIdFoTransferNft(address paramNftAddress, address paramTo, uint256 paramNftId, string memory signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public
+    {
+        worldId.verifyProof(root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof);
+        signalCounter++;
+
+        iNft(paramNftAddress).safeTransferFrom(address(this), paramTo, paramNftId);
+    }
+
+
+    function useWorldIdForLzCrossChainTransfer(address paramLzErc20Address, address paramDestinationAddressToTransfer, uint256 paramQuantityToTransfer, uint16 _dstChainId, string memory signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public payable
+    {
+        worldId.verifyProof(root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof);
+        signalCounter++;
+
+        iLzErc20(paramLzErc20Address).layerZeroTransfer{value: msg.value}(paramDestinationAddressToTransfer, paramQuantityToTransfer, _dstChainId);
+    }
+
+
+    function useWorldIdForfunctionLzNormalTransfer(address paramLzErc20Address, address paramTo, uint256 paramAmount, string memory signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public
+    {
+        iLzErc20(paramLzErc20Address).transfer(paramTo, paramAmount);
+
+        worldId.verifyProof(root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof);
+        signalCounter++;
+    }
+
+
+    function getStringSignalCounter() public view returns(string memory)
+    {
+        string memory stringSignal = Strings.toString(signalCounter);
+
+        return stringSignal;
+    }
+
+
+    /////"layerZeroIntegrationRelatedFunctions"////////////////////////////////////////////////////////////    
+
+    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory _srcAddress, uint64 _nonce, bytes memory _payload) internal override 
+    {
+        (address addressToTransfer, uint256 quantityToTransfer) = abi.decode(_payload, (address, uint256));
+
+        iLzErc20(usdTokenAddress).transfer(addressToTransfer, quantityToTransfer);
+    }
+
+
+    function estimateFee(uint16 _dstChainId, bytes calldata _payload, bool _useZro, bytes calldata _adapterParams) public view returns (uint nativeFee, uint zroFee) 
+    {
+        return lzEndpoint.estimateFees(_dstChainId, address(this), _payload, _useZro, _adapterParams);
+    }
+
+
+    function chainAbstractionTransferFunction(address paramDestinationAddressToTransfer, uint256 paramQuantityToTransferFromSourceAddress, uint256 paramQuantityToTransferFromDestinationAddress, uint16 _dstChainId) public payable 
+    {
+        bytes memory constructedPayloadDestination = abi.encode(paramDestinationAddressToTransfer, paramQuantityToTransferFromDestinationAddress);
+
+        uint16 valueOne = 1;
+        uint256 valueTwo = 200000;
+        bytes memory adapterParams = abi.encodePacked(valueOne, valueTwo);
+
+        uint etherToSend = 0.5 ether;
+
 
         _lzSend(_dstChainId, constructedPayloadDestination, payable(tx.origin), address(0x0), adapterParams, etherToSend);
         iLzErc20(usdTokenAddress).layerZeroTransfer{value: etherToSend}(paramDestinationAddressToTransfer, paramQuantityToTransferFromSourceAddress, _dstChainId);
     }
 
 
-
-
-
-
-    function setOracle(uint16 dstChainId, address oracle) external onlyOwner {
+    function setOracle(uint16 dstChainId, address oracle) external onlyOwner 
+    {
         uint TYPE_ORACLE = 6;
         // set the Oracle
         lzEndpoint.setConfig(lzEndpoint.getSendVersion(address(this)), dstChainId, TYPE_ORACLE, abi.encode(oracle));
     }
 
-    function getOracle(uint16 remoteChainId) external view returns (address _oracle) {
+    function getOracle(uint16 remoteChainId) external view returns (address _oracle) 
+    {
         bytes memory bytesOracle = lzEndpoint.getConfig(lzEndpoint.getSendVersion(address(this)), remoteChainId, address(this), 6);
-        assembly {
+        assembly 
+        {
             _oracle := mload(add(bytesOracle, 32))
         }
     }
 
    function makePathForThisContract(address paramRemoteAddress) public view returns(bytes memory)
    {
-    return abi.encodePacked(paramRemoteAddress, address(this));
+        return abi.encodePacked(paramRemoteAddress, address(this));
    }
 
     /////"nftRelatedFunctions"/////////////////////////////////////////////////////////////////////////////
@@ -218,9 +244,6 @@ contract abstractedAccount is NonblockingLzApp
 
     function lzCrossChainTransfer(address paramLzErc20Address, address paramDestinationAddressToTransfer, uint256 paramQuantityToTransfer, uint16 _dstChainId) public payable
     {
-        //function send(uint16 _dstChainId, bytes calldata _destination, bytes calldata _payload, address payable _refundAddress, address _zroPaymentAddress, bytes calldata _adapterParams) external payable;
-        //lzEndpoint.send{value: _nativeFee}(_dstChainId, trustedRemote, _payload, _refundAddress, _zroPaymentAddress, _adapterParams);
-
         iLzErc20(paramLzErc20Address).layerZeroTransfer{value: msg.value}(paramDestinationAddressToTransfer, paramQuantityToTransfer, _dstChainId);
     }
 
