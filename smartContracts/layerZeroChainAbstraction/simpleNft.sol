dab8a26233ae64d28b1ccf77298ea878556f6283
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-08-12 21:19:23 -0600

    Update simpleNft.sol

--- smartContracts/layerZeroChainAbstraction/simpleNft.sol
+++ smartContracts/layerZeroChainAbstraction/simpleNft.sol
@@ -1,1 +1,48 @@
-.
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract simpleNft is ERC721, Ownable
+{
+    modifier onlyAccounts() 
+    {
+        require(msg.sender == tx.origin, "Not allowed origin");
+        _;
+    }
+
+
+
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+
+    constructor() ERC721("simpleNft", "sNft")
+    {}
+
+
+    function Mint() external payable onlyAccounts 
+    {
+        mintInternal();
+    }
+
+    function mintInternal() internal
+    {
+        _tokenIds.increment();
+
+        uint256 tokenId = _tokenIds.current();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function totalSupply() public view returns (uint256) 
+    {
+        return _tokenIds.current();
+    }
+}
