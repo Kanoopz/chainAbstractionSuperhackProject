dd5cd65b9f1b996e79df95a179c92d137d023aac
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-08-13 10:43:19 -0600

    Update aaFactory.sol

--- smartContracts/layerZeroChainAbstraction/aaFactory.sol
+++ smartContracts/layerZeroChainAbstraction/aaFactory.sol
@@ -16,12 +16,6 @@ contract aaFactory
 
     mapping(address => abstractedAccount) public aaContractInstances;
 
-    /*
-        bool public worldIdPermitted;
-        IWorldID internal worldId;
-        uint256 internal externalNullifier;
-        uint256 internal immutable groupId = 1;
-    */
 
    ////////////////////////////////////////////////////////////////////////////
    /////"functions"////////////////////////////////////////////////////////////
@@ -31,8 +25,6 @@ contract aaFactory
 
    function createAa(address _lzEndpoint, address srcChainUsdTokenAddress) public returns(address)
    {
-        //address paramOwner, address paramFactoryAddress, address _lzEndpoint, address srcChainUsdTokenAddress
-
        abstractedAccount newAccount = new abstractedAccount(msg.sender, address(this), _lzEndpoint, srcChainUsdTokenAddress);
        address newAccountAddress = newAccount.getAbstractedAccountAddress();
        uint counterWithAccountCreated = ownerAaCounter[msg.sender] + 1;
@@ -48,57 +40,9 @@ contract aaFactory
 
        return newAccountAddress;
    }
-
-   /*
-    function createAaWithWorldId()
-    {}
-   */
    
    function getUserAccountByIndex(address paramUserAddress, uint paramIndex) public view returns(address)
    {
        return ownerAccounts[paramUserAddress][paramIndex];
    }
-
-
-
-
-
-
-   ////////////////////////////////////////////////////////////////////////////
-   /////"aaInstancesRelatedFunctions"///////////////////////////////////////////////////
-   ////////////////////////////////////////////////////////////////////////////
-   function aaInstanceGetName(address paramAaInstanceAddress, address paramNftAddress) public view returns(string memory)
-    {
-        return aaContractInstances[paramAaInstanceAddress].getNftName(paramNftAddress);
-    }
-
-    function aaInstanceMintNft(address paramAaInstanceAddress, address paramNftAddress) public
-    {
-        aaContractInstances[paramAaInstanceAddress].mintNft(paramNftAddress);
-    }
-
-    function aaInstancetransferNft(address paramAaInstanceAddress, address paramNftAddress, address paramTo, uint256 paramNftId) public
-    {
-        aaContractInstances[paramAaInstanceAddress].transferNft(paramNftAddress, paramTo, paramNftId);
-    }
-
-    function aaInstanceApproveNft(address paramAaInstanceAddress, address paramNftAddress, address paramNftTo, uint256 paramTokenId) public
-    {
-        aaContractInstances[paramAaInstanceAddress].approveNft(paramNftAddress, paramNftTo, paramTokenId);
-    }
-
-    function aaInstanceBalanceOfAbstractedAccount(address paramAaInstanceAddress, address paramNftAddress) public view returns(uint256)
-    {
-        aaContractInstances[paramAaInstanceAddress].balanceOfAbstractedAccount(paramNftAddress);
-    }
-
-    function aaInstanceCheckOwnerOf(address paramAaInstanceAddress, address paramNftAddress, uint256 paramNftId) public view returns(address)
-    {
-        aaContractInstances[paramAaInstanceAddress].checkOwnerOf(paramNftAddress, paramNftId);
-    }
-
-    function aaInstanceNftTotalSupply(address paramAaInstanceAddress, address paramNftAddress) public view returns(uint256)
-    {
-        aaContractInstances[paramAaInstanceAddress].nftTotalSupply(paramNftAddress);
-    }  
 }
